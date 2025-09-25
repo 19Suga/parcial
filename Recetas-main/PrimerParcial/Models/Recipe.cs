e72bd671b1b6f9c49fe8cc3de61896cff0a0f626
19Suga <russelluna4321@gmail.com>
2025-09-25 19:36:05 -0400

    crear modelo recipe

---
+++ Recetas-main/PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Title { get; set; } = "";
+
+    [Required]
+    public string Instructions { get; set; } = "";
+
+    [Display(Name = "Categoría")]
+    public int CategoryId { get; set; }
+
+    public Category? Category { get; set; }
+    public List<Ingredient> Ingredients { get; set; } = new();
+}
